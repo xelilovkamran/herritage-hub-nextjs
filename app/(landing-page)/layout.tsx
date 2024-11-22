94cab47c2a58ce7494c6fe3ee7ec0c005c3fdf0a
xelilovkamran <xlilovkamran@gmail.com>
2024-11-22 23:52:14 +0400

    refactor: remove unused code

--- app/(landing-page)/layout.tsx
+++ app/(landing-page)/layout.tsx
@@ -8,16 +8,15 @@ export default function Layout(props: { children: React.ReactNode }) {
         items={[
           { title: "Home", href: "/" },
           { title: "Explore", href: "/explore" },
-          // { title: "Github", href: "https://github.com/stack-auth/stack-template", external: true },
         ]}
       />
       <main className="flex-1">{props.children}</main>
       <Footer
         builtBy="Holberton School students"
         builtByLink="https://www.holbertonschool.com/"
-        githubLink="https://github.com/stack-auth/stack-template"
-        twitterLink="https://twitter.com/stack_auth"
-        linkedinLink="linkedin.com/company/stack-auth"
+        githubLink=""
+        twitterLink=""
+        linkedinLink=""
       />
     </div>
   );
